b7c97865cbd034024c56a796199c9a1f9f2ee9f8
Mcharvi <charvimehta006@gmail.com>
2026-02-11 22:29:26 +0530

    Walid parantesis

---
+++ Stacks/LC20.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class LC20 {
+    public static boolean isBalanced(String str){
+        Stack<Character> st = new Stack<>();
+        int n = str.length();
+     
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(isBalanced(str));
+    }
+}
+  
